ee2ef07917bfbbb7ba65305ef450f1fb98c11bcb
Mai Watanabe <mai881208@gmail.com>
2025-09-15 15:40:27 +0900

    ニュースのSSRを適用

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -6,6 +6,8 @@ import Pagination from "@/_components/ui/Pagination";
 import SearchField from "@/_components/ui/SearchField";
 import { Suspense } from "react";
 
+export const revalidate = 0;
+
 export default async function NewsPage() {
   const { contents: news, totalCount } = await getNewsList({
     limit: NEWS_LIST_LIMIT,
