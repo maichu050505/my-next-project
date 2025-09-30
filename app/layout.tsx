3aefc2b790d2acc1fdc6e0efbc4dd537b235e0ab
Mai Watanabe <mai881208@gmail.com>
2025-09-30 23:58:10 +0900

    Webフォントを削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
-import { Noto_Sans_JP } from "next/font/google";
 import type { Metadata } from "next";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/_components/layout/Header";
 import Footer from "@/_components/layout/Footer";
-
-const noto = Noto_Sans_JP({
-  subsets: ["latin"],
-  display: "swap",
-  adjustFontFallback: true,
-});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -49,7 +42,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja" className={`${geistSans.variable} ${geistMono.variable} ${noto.className}`}>
+    <html lang="ja" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body className="min-h-dvh bg-white text-gray-800">
         <Header />
         {children}
